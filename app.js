b5daabc64f7ea3f91910343a6bde5560d65af25e
originalchinmay <chinmayjj97@gmail.com>
2021-04-09 21:21:02 +0530

    added functions

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(textInput);
-console.log(outputDiv);
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// --------------random test
+// console.log(textInput);
+// console.log(outputDiv);
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
 
 function clickEventHandler() {
-  outputDiv.innerText = "jsjdfsd" + textInput.value;
+  // outputDiv.innerText = "jsjdfsd" + textInput.value;
+  //--------------taking input
+  var inputText = textInput.value;
+
+  fetch(getTranslationURL(inputText)).then((response) =>
+    response.json().then((json) => console.log(json))
+  );
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
