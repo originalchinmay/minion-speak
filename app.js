3d02ab564d507f0363f6b3e2fa9b88634af832a3
originalchinmay <chinmayjj97@gmail.com>
2021-04-09 10:18:32 +0530

    added button

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // var userName = prompt("Enter your pseudo name");
 // alert("This is working!! " + userName);
+
+//----------------button
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+  console.log("clicked!");
+});
